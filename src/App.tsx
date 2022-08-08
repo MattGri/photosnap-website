a9271c56b974474ca3ca35c5f85a028dc5be0304
Mateusz <mateusz.grzybowski98@gmail.com>
2022-08-08 08:21:10 +0200

    Added features

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Navigation from './components/Navigation';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Pricing from './pages/Pricing';
+import Features from './pages/Features';
 
 function App() {
     return (
@@ -14,6 +15,7 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Home />} />
 
+                    <Route path='/features' element={<Features />} />
                     <Route path="/pricing" element={<Pricing />} />
 
                 </Routes>
