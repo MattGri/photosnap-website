5c47f0edd7106c2e2e93ad99cf03cb63c9db8d3c
Mateusz <mateusz.grzybowski98@gmail.com>
2022-08-11 13:19:07 +0200

    Added header in home

--- src/App.tsx
+++ src/App.tsx
@@ -13,11 +13,9 @@ function App() {
             <div className="App">
                 <Navigation />
                 <Routes>
-                    <Route path="/" element={<Home />} />
-
+                    <Route path="/stories" element={<Home />} />
                     <Route path='/features' element={<Features />} />
                     <Route path="/pricing" element={<Pricing />} />
-
                 </Routes>
                 <Footer />
             </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React from 'react';
+import '../styles/home.scss';
+import { styled } from '@mui/material/styles';
+import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
 
 const Home = () => {
+
+
+  const CustomizedRightArrow = styled(ArrowRightAltIcon)`
+        color: #fff;
+        margin-Left: 10px;
+        cursor: pointer;
+    `;
+
   return (
-    <div>Home</div>
+    <>
+      <header className='wrapper'>
+        <div className='stories'>
+          <span className='line'></span>
+          <div className='container'>
+            <h1 className='title'>Create and
+              <br />
+              share your
+              <br />
+              photo stories.
+            </h1>
+            <p className='text'>
+              Photosnap is a platform for photopgraphers and visual
+              <br />
+              storytellers. We make it easy to share photos, tell
+              <br />
+              stories and connect with others.
+            </p>
+            <div className='inviteContainer'>
+              <button className='inviteButton'>get an invite</button>
+              <CustomizedRightArrow />
+            </div>
+          </div>
+        </div>
+        <div className='photoContainer'></div>
+      </header>
+    </>
   )
 }
 
